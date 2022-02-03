992d5d1d004eb7fccb6b9eb2da9c93fe4e0a8465
Matthew <me@matthw.dev>
2022-02-03 21:17:25 +0000

    syntax fixes

--- panel-nginx-conf.sh
+++ panel-nginx-conf.sh
@@ -1,11 +1,11 @@
 cleanup_and_copy() {
     rm /etc/nginx/sites-enabled/default
 
-    if [$1 -e "ssl"]; then
+    if ["$1" -e "ssl"]; then
         cp $(dirname $0)/assets/nginx.ssl.conf /etc/nginx/sites-available/pterodactyl.conf
     fi
 
-    if [$1 -e "nossl"]; then
+    if ["$1" -e "nossl"]; then
         cp $(dirname $0)/assets/nginx.nossl.conf /etc/nginx/sites-available/pterodactyl.conf
     fi
 
